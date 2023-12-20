ce04e917bfd3687f2d07c9e816e4cf6be74882cb
Suhiru <suhirudesilva@gmail.com>
2023-12-20 16:29:43 +0100

    a table added to view data and  login logout functionality added with registration

--- website/views.py
+++ website/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
+from .models import Record
 
 # Create your views here.
 
 def home(request):
+
+    records = Record.objects.all()
+
     #check to see if logging in
     if request.method == 'POST':
         username = request.POST['username']
@@ -20,7 +25,7 @@ def home(request):
             messages.success(request, "There was an error Logging In, Please try Again!")
             return redirect('home')
     else:   
-        return render(request,'home.html',{})
+        return render(request,'home.html',{'records': records})
 
 def logout_user(request):
     logout(request)
@@ -28,5 +33,19 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
-    return render(request,'register.html',{})
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			# Authenticate and login
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, "You Have Successfully Registered! Welcome!")
+			return redirect('home')
+	else:
+		form = SignUpForm()
+		return render(request, 'register.html', {'form':form})
 
+	return render(request, 'register.html', {'form':form})
