db11f45c88af939cbd66f1ba4130181d85a99836
Suhiru <suhirudesilva@gmail.com>
2023-12-21 12:16:49 +0100

    a user can be selected and deleted

--- website/views.py
+++ website/views.py
@@ -3,13 +3,13 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
 from .models import Record
+from pprint import pprint
 
 # Create your views here.
 
 def home(request):
 
     records = Record.objects.all()
-
     #check to see if logging in
     if request.method == 'POST':
         username = request.POST['username']
@@ -49,3 +49,26 @@ def register_user(request):
 		return render(request, 'register.html', {'form':form})
 
 	return render(request, 'register.html', {'form':form})
+
+def customer_record(request,pk):
+      if request.user.is_authenticated: 
+            customer_record = Record.objects.get(id=pk)
+            return render(request, 'record.html', {'customer_record':customer_record})
+      else:
+            messages.success(request,'You must be logged into to view that page!')
+            return redirect('home')
+    
+def delete_record(request,pk):
+
+    if request.user.is_authenticated:  
+            delete_it =  Record.objects.get(id=pk)
+            delete_it.delete()
+            messages.success(request, "You Have Successfully Deleted The Record!")
+            return redirect('home')
+    else:
+            messages.success(request, "You must be logged into do that")
+            return redirect('home')
+
+def add_record(request):
+     return render(request, 'add_record.html', {})
+            
